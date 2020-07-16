d7e4f1c5314fafdc66b8daed7cfae001d3a8eeea
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2020-07-16 03:31:07 +0200

    Can open and close groups and get object names

--- src/Data/HDF5.hs
+++ src/Data/HDF5.hs
@@ -1,22 +1,35 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Data.HDF5
   ( h5Open,
     h5Close,
+    h5Name,
+    h5Size,
+    h5OpenByIndex,
   )
 where
 
 import Data.HDF5.Internal
+import Foreign.C.String (peekCString)
+import Foreign.Marshal.Alloc (allocaBytes)
+import Foreign.Ptr (nullPtr)
+import Relude
 import System.Directory (doesFileExist)
 
--- import System.IO (IOMode (..))
+data H5Handle
+  = H5File !Hid
+  | H5Group !Hid
+  deriving stock (Eq, Show)
 
-newtype H5Handle = H5Handle {unH5Handle :: Hid}
-  deriving stock (Show)
-  deriving newtype (Eq)
+unHandle :: H5Handle -> Hid
+unHandle = \case
+  H5File h -> h
+  H5Group h -> h
 
-h5Check :: (MonadIO m, MonadFail m) => Maybe String -> Hid -> m Hid
-h5Check msg code
-  | code < 0 = fail $ fromMaybe ("failed with error code " <> show code) msg
-  | otherwise = return code
+-- h5Check :: (MonadIO m, MonadFail m) => Maybe String -> Hid -> m Hid
+-- h5Check msg code
+--   | code < 0 = fail $ fromMaybe ("failed with error code " <> show code) msg
+--   | otherwise = return code
 
 h5Open :: (MonadIO m, MonadFail m) => FilePath -> IOMode -> m H5Handle
 h5Open path mode = do
@@ -25,12 +38,61 @@ h5Open path mode = do
       doesFileExist path >>= \case
         True ->
           let flags = if mode == ReadMode then H5F_ACC_RDONLY else H5F_ACC_RDWR
-           in h5_open path flags h5_DEFAULT
-        False -> h5_create path H5F_ACC_EXCL h5_DEFAULT h5_DEFAULT
+           in h5f_open path flags H5P_DEFAULT
+        False -> do
+          when (mode == ReadMode || mode == AppendMode) $
+            fail $ "file " <> show path <> " not found"
+          h5f_create path H5F_ACC_EXCL H5P_DEFAULT H5P_DEFAULT
   when (r < 0) $ fail ("error opening file: " <> show path <> "; error code: " <> show r)
-  return (H5Handle r)
+  return $ H5File r
 
 h5Close :: (MonadIO m, MonadFail m) => H5Handle -> m ()
-h5Close handle = do
-  status <- liftIO $ h5_close (unH5Handle handle)
-  when (status < 0) $ fail "error closing file"
+h5Close = \case
+  H5File handle -> do
+    status <- liftIO $ h5f_close handle
+    when (status < 0) $ fail "error closing file"
+  H5Group handle -> do
+    status <- liftIO $ h5o_close handle
+    when (status < 0) $ fail "error closing group"
+
+checkValidHandle :: (MonadFail m, MonadIO m) => H5Handle -> m ()
+checkValidHandle handle
+  | h < 0 = fail $ "handle is invalid: " <> show handle
+  | otherwise =
+    (liftIO $ h5i_is_valid h) >>= \status -> case compare status 0 of
+      LT -> fail $ "H5Iis_valid failed with error code " <> show status
+      EQ -> fail $ "handle is invalid: " <> show handle
+      _ -> return ()
+  where
+    h = unHandle handle
+
+h5Size :: (MonadIO m, MonadFail m) => H5Handle -> m Int
+h5Size g =
+  checkValidHandle g >> do
+    r <- liftIO . h5g_get_num_objs . unHandle $ g
+    when (r < 0) . fail $ "error getting group size; error code: " <> show r
+    return r
+
+h5Name :: (MonadIO m, MonadFail m) => H5Handle -> m String
+h5Name handle =
+  let h = unHandle handle
+   in checkValidHandle handle >> do
+        liftIO (h5i_get_name h nullPtr 0) >>= \r -> case compare r 0 of
+          GT -> liftIO $
+            allocaBytes (r + 1) $ \s -> do
+              status <- h5i_get_name h s (r + 1)
+              when (status < 0) $ failWithCode status
+              peekCString s
+          EQ -> return ""
+          LT -> failWithCode r
+  where
+    failWithCode e = fail $ "error getting object's name; error code: " <> show e
+
+h5OpenByIndex :: (MonadIO m, MonadFail m) => H5Handle -> Int -> m H5Handle
+h5OpenByIndex parent index =
+  checkValidHandle parent >> do
+    groupSize <- h5Size parent
+    when (index < 0 || index >= groupSize) . fail $ "invalid index: " <> show index
+    r <- liftIO $ h5o_open_by_idx (unHandle parent) "." H5_INDEX_NAME H5_ITER_INC index H5P_DEFAULT
+    when (r < 0) . fail $ "error accessing object at index " <> show index
+    return $ H5Group r
--- src/Data/HDF5/Internal.chs
+++ src/Data/HDF5/Internal.chs
@@ -1,8 +1,14 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Data.HDF5.Internal where
 
--- import Foreign.C.Types (CUInt)
+import Relude hiding (error)
+import Prelude (error)
+import Foreign.Ptr (Ptr)
+import Foreign.Marshal.Alloc (allocaBytesAligned)
+import Foreign.Storable (Storable(..))
+import Foreign.C.Types (CChar)
 
 #include <hdf5.h>
 #include <hdf5_hl.h>
@@ -16,42 +22,72 @@ type Hbool = {#type hbool_t#}
 
 {#typedef herr_t Herr#}
 {#typedef hid_t Hid#}
-{#typedef hsize_t Hsize#}
-{#typedef hssize_t Hssize#}
+-- {#typedef hsize_t Hsize#}
+-- {#typedef hssize_t Hssize#}
 {#typedef htri_t Htri#}
 {#typedef hbool_t Hbool#}
 
-h5_DEFAULT :: Hid
-h5_DEFAULT = 0
+data H5P_DEFAULT = H5P_DEFAULT
 
-data H5F_ACC = H5F_ACC_RDONLY
-             | H5F_ACC_RDWR
-             | H5F_ACC_TRUNC
-             | H5F_ACC_EXCL
-             | H5F_ACC_DEBUG
-             | H5F_ACC_CREAT
-             | H5F_ACC_SWMR_WRITE
-             | H5F_ACC_SWMR_READ
+instance Enum H5P_DEFAULT where
+  toEnum = undefined
+  fromEnum H5P_DEFAULT = 0
+
+{#default in `H5P_DEFAULT' [hid_t] withEnum#}
+
+
+data H5F_ACC
+  = H5F_ACC_RDONLY
+  | H5F_ACC_RDWR
+  | H5F_ACC_TRUNC
+  | H5F_ACC_EXCL
+  | H5F_ACC_DEBUG
+  | H5F_ACC_CREAT
+  | H5F_ACC_SWMR_WRITE
+  | H5F_ACC_SWMR_READ
 
 instance Enum H5F_ACC where
   toEnum = undefined
   fromEnum = \case
-    H5F_ACC_RDONLY     -> 0x0000
-    H5F_ACC_RDWR       -> 0x0001
-    H5F_ACC_TRUNC      -> 0x0002
-    H5F_ACC_EXCL       -> 0x0004
-    H5F_ACC_DEBUG      -> 0x0008
-    H5F_ACC_CREAT      -> 0x0010
+    H5F_ACC_RDONLY -> 0x0000
+    H5F_ACC_RDWR -> 0x0001
+    H5F_ACC_TRUNC -> 0x0002
+    H5F_ACC_EXCL -> 0x0004
+    H5F_ACC_DEBUG -> 0x0008
+    H5F_ACC_CREAT -> 0x0010
     H5F_ACC_SWMR_WRITE -> 0x0020
-    H5F_ACC_SWMR_READ  -> 0x0040
+    H5F_ACC_SWMR_READ -> 0x0040
 
+{#enum H5_iter_order_t {} #}
+{#enum H5_index_t {} #}
+
+withEnum :: (Enum a, Integral b) => a -> b
+withEnum = fromIntegral . fromEnum
 
 -- hid_t H5Fopen( const char *name, unsigned flags, hid_t fapl_id )
-{#fun H5Fopen as h5_open { `String', withEnum `H5F_ACC', `Hid' } -> `Hid' #}
-  where withEnum = fromIntegral . fromEnum
+{#fun H5Fopen as h5f_open { `String', withEnum `H5F_ACC', `H5P_DEFAULT' } -> `Hid' #}
 -- hid_t H5Fcreate( const char *name, unsigned flags, hid_t fcpl_id, hid_t fapl_id )
-{#fun H5Fcreate as h5_create { `String', withEnum `H5F_ACC', `Hid', `Hid' } -> `Hid' #}
-  where withEnum = fromIntegral . fromEnum
+{#fun H5Fcreate as h5f_create { `String', withEnum `H5F_ACC', `H5P_DEFAULT', `H5P_DEFAULT' } -> `Hid' #}
 -- herr_t H5Fclose( hid_t file_id )
-{#fun H5Fclose as h5_close { `Hid' } -> `Herr' #}
+{#fun H5Fclose as h5f_close { `Hid' } -> `Herr' #}
+-- hid_t H5Oopen_by_idx( hid_t loc_id, const char *group_name, H5_index_t index_type, H5_iter_order_t order, hsize_t n, hid_t lapl_id )
+{#fun H5Oopen_by_idx as h5o_open_by_idx { `Hid', `String', `H5_index_t', `H5_iter_order_t', `Int', `H5P_DEFAULT' } -> `Hid' #}
+{#fun H5Oclose as h5o_close { `Hid' } -> `Herr' #}
+-- htri_t H5Iis_valid( hid_t obj_id )
+{#fun H5Iis_valid as h5i_is_valid { `Hid' } -> `Htri' #}
+{#fun H5Iget_name as h5i_get_name { `Hid', id `Ptr CChar', `Int' } -> `Int' #}
+-- herr_t H5Gget_info( hid_t group_id, H5G_info_t *group_info )
+{#fun H5Gget_info as h5g_get_info' { `Hid', `Ptr ()' } -> `Herr' #}
+-- H5Gget_num_objs is deprecated in favour of H5Gget_info, so we use it to
+-- obtain the number of objects within a group. Note that this function uses
+-- error codes: negative return value indicates error.
+h5g_get_num_objs :: Hid -> IO Int
+h5g_get_num_objs groupId =
+  allocaBytesAligned {#sizeof H5G_info_t#} {#alignof H5G_info_t#} $ \ptr -> do
+    status <- h5g_get_info' groupId ptr
+    if status < 0
+      then return $ fromIntegral status
+      else do
+        count <- peekByteOff ptr {#offsetof H5G_info_t->nlinks#} :: IO {#type hsize_t#}
+        return $ fromIntegral count
 
